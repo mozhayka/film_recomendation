031547bd3ba776ffb6c1ac16d5f4ead83f08e3b9
mozhayka <mozhay2000@gmail.com>
2023-11-13 15:30:33 +0300

    Обновил архитектуру, добавил папку под парсер на Go

---
+++ src/Python/site_requests/interfaces.py
@@ -0,0 +1,5 @@
+from src.Python.main_body.structures import FilmId, Vertex
+
+
+# Парсит json-ы, которые возвращает проект на Go
+def get_from_site(id: FilmId) -> Vertex
--- src/site_requests/interfaces.py
+++
@@ -1,5 +0,0 @@
-from src.main_body.structures import FilmId, Vertex
-
-
-# Создает реквест на сайт, возвращает список похожих фильмов. Тут может быть реализована связь с проектом на Go
-def get_from_site(id: FilmId) -> Vertex
