b140dc0251ed4275d3bb3c11c2434466cbdc129d
mozhayka <mozhay2000@gmail.com>
2023-11-13 15:10:14 +0300

    Интерфейсы

--- src/main_body/structures.py
+++ src/main_body/structures.py
@@ -2,16 +2,20 @@ from typing import List
 
 from dataclasses import dataclass
 
+# Чтобы различать фильмы, можем хранить названия и url (или несколько url для нескольких сайтов)
+# Возможно в будущем добавление актерского состава например
 @dataclass
 class FilmId:
     name: str
     url: str
 
+# Это вершины дерева, по которому будем ходить
 @dataclass
 class Vertex:
     val: FilmId
     similar: List<FilmId>
 
+# То, что возвращаем пользователю
 @dataclass
 class Answer:
     recommended_films: List<FilmId>
