b60ee96bda17811d2db1be1ff9842f02639ac369
Andrey Svistukhin <a.svistukhin@just-ai.com>
2023-11-15 23:27:06 +0300

    get_request wrapper init

--- src/parser_requests/interfaces.py
+++ src/parser_requests/interfaces.py
@@ -1,11 +1,14 @@
+from datetime import datetime, timedelta
+
 from src.conn import conn
-from src.db.film_dao import save_to_base, get_from_base
-from src.structures import FilmId, VertexDto
 from typing import List
 
 import requests
 from src.db.film_dao import save_to_database, get_from_database
 from src.structures import FilmId, VertexDto
+
+MAX_REQUEST_TIMEOUT = 1200
+DEFAULT_REQUEST_TIMEOUT = 20
 
 
 def json_to_vertex(json, mode: str) -> VertexDto:
@@ -17,23 +20,36 @@ def json_to_vertex(json, mode: str) -> VertexDto:
     return VertexDto(val=film_id, source=mode, similar=recommended_movies)
 
 
-# Делает GET запросы к парсеру на Go, парсит полученные json-ы и возвращает Vertex
-def get_request(film: FilmId, mode: str='ivi') -> VertexDto:
-    vertex = get_from_database(film, conn)
-    if vertex is None:
-        params = {"by": "link", "query": film.url}
-        response = requests.get(f'http://parser:8080/{mode}/films', json=params)
-        if response.status_code != 200:
-            print(response.json())
-            # return VertexDto  # TODO: return error form json response
-
-        json_response = response.json()
-        vertex = json_to_vertex(json_response, mode)
-        save_to_database(vertex, conn)
+def get_request(film_id: FilmId, mode: str = 'ivi') -> VertexDto:
+    vertex = get_from_database_with_timeout(film_id, mode)
+    if vertex is not None:
         return vertex
-    # else:
-    #     vertex.similar = [FilmId(name=v['name'], url=v['url']) for v in vertex.similar]
 
+    return get_recommended_films(film_id, mode)
+
+
+def get_from_database_with_timeout(film_id: FilmId, mode: str):
+    vertex = get_from_database(film_id, conn)
+
+    if vertex is not None and vertex.updated_at:
+        time_difference = datetime.utcnow() - vertex.updated_at
+        if time_difference > timedelta(weeks=1):
+            try:
+                vertex = get_recommended_films(film_id, mode, timeout=DEFAULT_REQUEST_TIMEOUT)
+            except requests.Timeout:
+                return vertex
+
+    return vertex
+
+
+def get_recommended_films(film_id: FilmId, mode: str = 'ivi', timeout=MAX_REQUEST_TIMEOUT) -> VertexDto:
+    params = {"by": "link", "query": film_id.url}
+    response = requests.get(f'http://parser:8080/{mode}/films', json=params, timeout=timeout)
+    response.raise_for_status()  # throw exception if status not 200
+
+    json_response = response.json()
+    vertex = json_to_vertex(json_response, mode)
+    save_to_database(vertex, conn)
     return vertex
 
 
