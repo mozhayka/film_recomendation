ffbb709a0630ca643e1ea20255b4082c3f708c26
mozhayka <mozhay2000@gmail.com>
2023-11-15 11:15:59 +0300

    simple test

--- test/algo/simple.py
+++ test/algo/simple.py
@@ -1,19 +1,11 @@
-from src.structures import FilmId, Vertex
+from src.alogrithms.algo import do_search
+from src.structures import FilmId
 
 
-f1 = FilmId(name="F1", url="url1")
-f2 = FilmId(name="F2", url="url2")
-f3 = FilmId(name="F3", url="url3")
-f4 = FilmId(name="F4", url="url4")
-f5 = FilmId(name="F5", url="url5")
-f6 = FilmId(name="F6", url="url6")
+f1 = FilmId(name="Брат 2", url="")
+f2 = FilmId(name="57 секунд", url="")
 
 
-vertexes = [
-    Vertex(val=f1, similar=[f2, f3]),
-    Vertex(val=f2, similar=[f3, f4]),
-    Vertex(val=f3, similar=[f2, f4]),
-    Vertex(val=f4, similar=[f3, f5, f6]),
-    Vertex(val=f5, similar=[]),
-    Vertex(val=f6, similar=[f4]),
-]
+async def test1():
+    ans = await do_search(f1, f2)
+    print(ans.recommended_films)
