6a1d67e212256b406ff0c86de61926d5d649554e
mozhayka <mozhay2000@gmail.com>
2023-11-15 16:17:24 +0300

    remove

--- test/main.py
+++
@@ -1,3 +0,0 @@
-from test.simple import test1
-
-test1()
--- test/simple.py
+++
@@ -1,7 +0,0 @@
-from test.algo_test import do_search_test
-from test.test_data import f1, f2
-
-
-async def test1():
-    ans = await do_search_test(f1, f2)
-    print(ans.recommended_films)
