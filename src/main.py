93fc656a39214fec2a0689e6afd44e0de0f190f4
mozhayka <mozhay2000@gmail.com>
2023-11-15 11:23:26 +0300

    delete useless code

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from src.alogrithms.algo import do_search
-from src.telegram_bot.interfaces import read_input, print_answer
-
-
-# Скорее всего центр программы должен находиться внутри бота, оттуда соответственно идут вызовы do_search()
-# так что эта часть будет переписана
-def main():
-    [film1, film2] = read_input()
-    print_answer(do_search(film1, film2))
